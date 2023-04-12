a2a1bca3e987aa10f20eb080e2788e39f5f9a3eb
amosreginaldjr <98925229+amosreginaldjr@users.noreply.github.com>
2023-04-12 11:06:19 -0400

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -20,18 +20,11 @@ private:
 
 public:
 	Tree();
-	Tree(TreeNode* root_node);
 	void insert(Tdata user_input);
 };
 
 template<typename Tdata>
 Tree<Tdata>::Tree()
-{
-
-}
-
-template<typename Tdata>
-Tree<Tdata>::Tree(TreeNode* root_node)
 {
 	root_node = new TreeNode;
 	root_node->data = NULL;
@@ -43,17 +36,46 @@ template<typename Tdata>
 void Tree<Tdata>::insert(Tdata user_input)
 {
 	TreeNode* newNode = new TreeNode;
-
+	//base case:
 	if (root_node->data == NULL)
+	{
 		root_node->data = user_input;
+		root_node->left = nullptr;
+		root_node->right = nullptr;
+		cout << root_node->data << endl;
+	}
+	
 
+	
 	//TreeNode* temp = root_node;
+	if (user_input < root_node->data)
+	{
+		if (root_node->left == nullptr)
+		{
+			root_node->left = newNode;
+			newNode->data = user_input;
+			newNode->left = nullptr;
+			newNode->right = nullptr;
+			cout << root_node->left->data << endl;
+		}
+		else if (root_node->left != nullptr)
+		{
+			while (!root_node->left)
+			{
+
+
+			}
+		}
+	}
+
 }
 
 int main()
 {
 	Tree<int> tt;
 	tt.insert(5);
+	//tt.insert(7);
+	tt.insert(3);
 
 	return 0;
 }
